21d1592e5b7c3d20edba4c7371474fc8e439feae
Eric Chang <pkerichang@gmail.com>
2018-10-24 08:41:22 -0700

    change setup so that pybind11 is a submodule, and scripts have better PYTHONPATH configuration.

--- gen_stubs_golden.sh
+++ gen_stubs_golden.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # this script generates .pyi interface files for Pycharm using mypy
 
-export PYTHONPATH="src_python"
+export PYTHONPATH="src_python${PYTHONPATH:+:$PYTHONPATH}"
 python -m mypy.stubgen --recursive -o stubs_golden example
--- gen_stubs_old.sh
+++ gen_stubs_old.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # this script generates .pyi interface files for Pycharm using mypy
 
-export PYTHONPATH="build/lib.linux-x86_64-3.7"
+export PYTHONPATH="build/lib.linux-x86_64-3.7${PYTHONPATH:+:$PYTHONPATH}"
 python -m mypy.stubgen --recursive -o stubs_old example
 python -m mypy.stubgen --recursive -o stubs_old example.subpackage
 python -m mypy.stubgen --recursive -o stubs_old example.subpackage.submodule
